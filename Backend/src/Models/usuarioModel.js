67030c16321a93cff0c2716f3c9a39cef5533c75
João Victor Matos Francisco <168545335+JoaoVFrancisco@users.noreply.github.com>
2025-05-19 11:19:16 -0300

    começando o backend e fizemos uma parte do banco de dados

---
+++ Backend/src/Models/usuarioModel.js
@@ -0,0 +1,23 @@
+import db from '../db.js';
+import mysql from 'mysql2/promise';
+
+const conexao = mysql.createPool(db);
+
+export const criandousuario = async (nome, login, senha) => {
+    console.log("usuarioModel :: criandousuario")
+    const sql = `INSERT INTO Usuario (nome, login, senha) VALUES (?, ?, ?`;
+
+    const params = [nome, login, senha];
+
+    try {
+        const [resposta] = await console.query(sql, params);
+        return[201, {mensagem: 'usuario criado com sucesso!!'}]
+    } catch (error) {
+        console.error({mensagem: "Error ao criar Usuario", code:error.code, sql:error.sqlMessage});
+    }
+};
+
+export const exibirusuario = async () => {
+
+}
+
